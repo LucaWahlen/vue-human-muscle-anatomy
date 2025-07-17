d50cc446489f356c476d98db472c0678ce5a5fcf
Luca Wahlen <mail@lucawahlen.de>
2025-07-17 21:12:30 +0200

    setup project

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import HumanMuscleAnatomy from "./components/HumanMuscleAnatomy.vue";
+import type { MuscleGroup } from './components/HumanMuscleAnatomy.vue'
+
+export { HumanMuscleAnatomy, type MuscleGroup};
